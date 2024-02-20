13d9d5b1214d1f8ab94aca998cc247894d489f43
Rytis <138163190+RytisGL@users.noreply.github.com>
2024-02-20 21:41:45 +0200

    Missing println added. Missing "a" has been located and reintroduced into the sentence.

--- trees/BirchTree.java
+++ trees/BirchTree.java
@@ -8,7 +8,7 @@ public class BirchTree extends Deciduous {
         return "Birch tree ";
     }
     public static void birchTreeForest(List<BirchTree> forestList) {
-        System.out.println("It is birch tree forest");
+        System.out.println("It is a birch tree forest");
         for (BirchTree b : forestList) {
             b.have();
         }
--- trees/ThornTree.java
+++ trees/ThornTree.java
@@ -9,6 +9,7 @@ public class ThornTree implements Tree {
         System.out.println(this + "has spikes.");
     }
     public static void thornTreeForest(List<? extends ThornTree> forestList) {
+        System.out.println("It is a thorn tree forest");
         for (ThornTree c : forestList) {
             c.have();
         }
