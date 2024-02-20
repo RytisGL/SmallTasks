f711a2c9d110f506ce5a2b41ff02a4c62e1bebe3
Rytis <138163190+RytisGL@users.noreply.github.com>
2024-02-20 20:47:17 +0200

    Juniper have method fixed.

--- trees/Juniper.java
+++ trees/Juniper.java
@@ -3,6 +3,6 @@ package trees;
 public class Juniper extends Conifer {
     @Override
     public void have() {
-        System.out.println("Trees.Juniper " + getBranchTypeString());
+        System.out.println("Juniper " + getBranchTypeString());
     }
 }
