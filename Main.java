c863c1e6bb7790ac76d0dc24ce156cc7794e06a3
Rytis <138163190+RytisGL@users.noreply.github.com>
2024-02-20 19:19:04 +0200

    Array instead of list -.-

--- Main.java
+++ Main.java
@@ -1,17 +1,13 @@
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> stringL = new ArrayList<>();
-        stringL.add("String");
-        List<Integer> integerList = new ArrayList<>();
-        integerList.add(1);
+        String[] stringL = {"String"};
+        Integer[] integerList = {1};
         printList(stringL);
         printList(integerList);
     }
 
-    public static void printList(List<?> l) {
+    public static <T> void printList(T[] l) {
         for (Object o : l) {
             System.out.println(o);
         }
