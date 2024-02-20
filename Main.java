c330faa8a4be6ec5a4ea7580aea1dfab817ddf00
Rytis <138163190+RytisGL@users.noreply.github.com>
2024-02-20 19:10:09 +0200

    Main java fixed

--- Main.java
+++ Main.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class main {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> stringL = new ArrayList<>();
+        stringL.add("String");
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(1);
+        printList(stringL);
+        printList(integerList);
+    }
+
+    public static void printList(List<?> l) {
+        for (Object o : l) {
+            System.out.println(o);
+        }
+    }
 }
