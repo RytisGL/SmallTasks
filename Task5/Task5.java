16c21b40f67bf97966536490b2c0ee74e710dd2f
Rytis <138163190+RytisGL@users.noreply.github.com>
2024-02-22 19:57:19 +0200

    Extra task, ne pats optimaliausias, bet orginalus! :)

--- Task5/Task5.java
+++ Task5/Task5.java
@@ -1,8 +1,33 @@
 package Task5;
 
+import java.util.*;
+
 public class Task5 {
     public static void main(String[] args) {
         SuperPair<String, String, Integer> superPair = new SuperPair<>("String", "String", 123);
 //        SuperPair<String, String, Integer> superPair1 = new SuperPair<>("String", "String", "String");
+        int[] ex =  {1,3,6,4,1,2};
+        System.out.println(solution(ex));
+    }
+    public static int solution(int[] a) {
+        int check = 1;
+        HashSet<Integer> removeDup = new HashSet<>();
+        List<Integer> sort = new ArrayList<>();
+        for (int num: a) {
+            removeDup.add(num);
+        }
+        sort.addAll(removeDup);
+        Collections.sort(sort);
+        for (int i = 0; i < sort.size(); i++) {
+            if (sort.get(i) >= 1 && sort.get(i) != check) {
+                return check;
+            }
+            if (sort.get(i) < 1) {
+                check = 1;
+            } else {
+                check++;
+            }
+        }
+        return check;
     }
 }
