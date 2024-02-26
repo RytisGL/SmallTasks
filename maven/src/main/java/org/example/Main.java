e4d7d6320e49fce42da7ad2563237bdbec7b9358
Rytis <138163190+RytisGL@users.noreply.github.com>
2024-02-26 21:28:59 +0200

    Task1 folder name now 100% fixed

--- maven/src/main/java/org/example/Main.java
+++ maven/src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import com.google.common.collect.Range;
+import com.google.common.primitives.Ints;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,5 +12,12 @@ public class Main {
         System.out.println(open.contains(23));
         System.out.println(closed.span(open));
         System.out.println(closed.intersection(open));
+        int[] ints = {1,2,3,4,5,6};
+        System.out.println(Ints.min(ints));
+        System.out.println(Ints.max(ints));
+        System.out.println(Ints.contains(ints, 1));
+        Integer[] ints2 = new Integer[ints.length];
+        Ints.asList(ints).toArray(ints2);
+        System.out.println(ints2[1]);
     }
 }
